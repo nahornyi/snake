8b33de74a5209d3792aff4f140420d2c4b1dc6fe
Nagornyi <vnagornyi1989@gmail.com>
2016-02-17 14:51:53 +0200

    Создание класса Point. Создание метода DrawSymbol

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,27 +10,26 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Point p1 = new Point();
 
-            int x1 = 1;
-            int y1 = 3;
-            char symbol1 = '*';
-            DrawSymbol( x1, y1, symbol1 );
-           
+            p1.x = 1;
+            p1.y = 2;
+            p1.symbol = '*';
+            p1.DrawSymbol();
 
-            int x2 = 4;
-            int y2 = 5;
-            char symbol2 = '#';
-            DrawSymbol( x2, y2, symbol2 );
-           
+            Point p2 = new Point();
+
+            p2.x = 4;
+            p2.y = 5;
+            p2.symbol = '#';
+            p2.DrawSymbol();
 
             Console.ReadKey();
         }
 
-        static void DrawSymbol(int x, int y, char symbol)
-        {
-            Console.SetCursorPosition( x, y );
-            Console.Write( symbol );
-        }
+     
+
+        
 
     }
 }
