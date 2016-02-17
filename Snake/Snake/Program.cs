b940972d23bd7c52bf6cb9cfb313ee9806280a4d
Nagornyi <vnagornyi1989@gmail.com>
2016-02-17 14:24:03 +0200

    Процедурный подход. Функция DrawSymbol

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,8 +11,26 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello world!");
-            Console.ReadLine();
+            int x1 = 1;
+            int y1 = 3;
+            char symbol1 = '*';
+            DrawSymbol( x1, y1, symbol1 );
+           
+
+            int x2 = 4;
+            int y2 = 5;
+            char symbol2 = '#';
+            DrawSymbol( x2, y2, symbol2 );
+           
+
+            Console.ReadKey();
         }
+
+        static void DrawSymbol(int x, int y, char symbol)
+        {
+            Console.SetCursorPosition( x, y );
+            Console.Write( symbol );
+        }
+
     }
 }
