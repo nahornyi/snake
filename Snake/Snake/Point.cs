e7c62d57e70d3c48734fbccceaadd4a1cf2aadb7
Nagornyi <vnagornyi1989@gmail.com>
2016-02-18 12:17:30 +0200

    Создание класса Змейка

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -22,10 +22,43 @@ namespace Snake
             symbol = _symbol;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            symbol = p.symbol;
+        }
+
+        public void Move(int offset, Direction direction) 
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x = x + offset;
+            }
+            else if (direction == Direction.LEFT) 
+            {
+                x = x - offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y = y + offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y = y - offset;
+            }
+
+        }
+
         public void DrawSymbol()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(symbol);
+        }
+
+        public override string ToString()
+        {
+            return x + "," + y + "," + symbol; 
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -23,7 +23,9 @@ namespace Snake
             vl.DrawLine();
 
 
-                
+            Point p = new Point(4,5,'*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.DrawLine();
 
             Console.ReadKey();
         }
