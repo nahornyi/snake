024f526e87eac8e625d177ebf711e003335e9bbd
Nagornyi <vnagornyi1989@gmail.com>
2016-02-18 09:55:53 +0200

    Классы горизонтальная и вертикальная линия

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine
+    {
+
+        List<Point> pList;
+
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int i = xLeft; i <= xRight; i++)
+            {
+                Point p = new Point(i,y,sym);
+                pList.Add(p);
+            }
+
+            
+           
+
+            
+        }
+
+
+        public void DrawLine()
+        {
+            foreach (Point p in pList)
+            {
+                p.DrawSymbol();
+            }
+        }
+
+
+    }
+}
---
+++ Snake/Snake/VerticalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VerticalLine
+    {
+        List<Point> pVerticalList;
+        public VerticalLine(int x, int yTop, int yBottom, char sym)
+        { 
+            pVerticalList = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x,y,sym);
+                pVerticalList.Add(p);
+            }
+                
+        }
+        public void DrawLine()
+        {
+            foreach (Point p in pVerticalList)
+            {
+                p.DrawSymbol();
+            }
+        }
+
+    }
+}
